72001a9ba28937d70e5a9c9aad957f0add16792d
scottsak <48599109+scottsak@users.noreply.github.com>
2020-12-13 22:11:05 -0800

    player moves now

--- race.py
+++ race.py
@@ -3,6 +3,7 @@ import pygame
 #setup display
 pygame.init()
 WIDTH, HEIGHT= 1000,1000
+GREEN = (75,139,59)
 
 #creates window
 win = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -14,15 +15,29 @@ clock = pygame.time.Clock()
 images = []
 for i in range(2):
     player = pygame.image.load("player"+str(i)+".png")
-    player = pygame.transform.rotozoom(player, 0, 5)
+    player = pygame.transform.scale(player, (200, 200))
     images.append(player)
+
+#creates buttons
+startButton = pygame.Rect(100,100,50,50)
+
+
+#starts race
+def startRace():
+    global p1x
+    while (p1x<100):
+        p1x+=1
+        win.blit(images[0],(p1x,p2y))
+        pygame.display.flip()
 
 #keeps the game going
 run = True
+p1x, p2y =  0,0
 while run:
     clock.tick(FPS)
-    win.fill((255,255,255))
-    win.blit(images[0],(1,1))
+    win.fill(GREEN)
+    win.blit(images[0],(p1x,p2y))
+    pygame.draw.rect(win, (200,0,0),startButton)
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -30,7 +45,10 @@ while run:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(pos)
+
+            #will start race
+            if startButton.collidepoint(pos):
+                startRace()
 
 
 pygame.quit()
