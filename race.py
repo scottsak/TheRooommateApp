0dc1c336fb397f6b2706c1a8b97cc75adeedd01b
scottsak <48599109+scottsak@users.noreply.github.com>
2020-12-15 22:52:17 -0800

    changed so players move at random amounts to make race results different

--- race.py
+++ race.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #setup display
 pygame.init()
@@ -21,32 +22,57 @@ for i in range(2):
 #creates buttons
 startButton = pygame.Rect(500,500,50,50)
 
-
+#creates finish line
+finishlineY = 0
+def createFinishLine():
+    global win
+    global HEIGHT
+    white=True
+    global finishlineY
+    i=0
+    for i in range(HEIGHT):
+        if white:
+            pygame.draw.rect(win, (255,255,255), pygame.Rect(500,finishlineY,50,50))
+            white = False
+        else:
+            pygame.draw.rect(win,(0,0,0),pygame.Rect(500,finishlineY,50,50))
+            white = True
+        i +=50
+        finishlineY +=50
 
 
 #keeps the game going
 run = True
-p1x, p2y =  0,0
+p1y,p2y = 0,200
+p1x,p2x = 0,0
+
 while run:
     clock.tick(FPS)
     win.fill(GREEN)
-    win.blit(images[0],(p1x,p2y))
+    win.blit(images[0],(p1x,p1y))
+    win.blit(images[1],(p2x,p2y))
     pygame.draw.rect(win, (200,0,200),startButton)
+    #createFinishLine()
+
     pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if ev.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
 
             #will start race
             if startButton.collidepoint(pos):
+                p1x,p2x=0,0
+                win.blit(images[0],(p1x,p1y))
+                win.blit(images[1],(p2x,p2y))
                 while(p1x<500):
-                    p1x+=1
+                    p1x+=random.randint(0,5)
+                    p2x+= random.randint(0,5)
                     win.fill(GREEN)
-                    win.blit(images[0],(p1x,p2y))
+                    createFinishLine()
+                    win.blit(images[0],(p1x,p1y))
+                    win.blit(images[1],(p2x,p2y))
                     pygame.display.update()
-
-
 pygame.quit()
