cfb93d4de0814ce138c47020b9c62c3269bf0fba
scottsak <48599109+scottsak@users.noreply.github.com>
2020-12-19 20:27:29 -0800

    switched finish line and other things

--- race.py
+++ race.py
@@ -5,6 +5,7 @@ import random
 pygame.init()
 WIDTH, HEIGHT= 1000,1000
 GREEN = (75,139,59)
+WHITE = (255,255,255)
 
 #creates window
 win = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -21,11 +22,12 @@ for i in range(2):
 
 #creates finish line
 finishline = pygame.image.load("finishline.png")
-finishline = pygame.transform.rotate(finishline,90)
+finishline = pygame.transform.rotate(finishline,270)
 finishline = pygame.transform.scale(finishline,(200,1000))
 
 #creates buttons
-startButton = pygame.Rect(500,500,50,50)
+startButton = pygame.Rect(100,800,300,50)
+    
 
 #creates background
 def createBackground():
@@ -33,8 +35,13 @@ def createBackground():
     win.fill(GREEN)
     win.blit(images[0],(p1x,p1y))
     win.blit(images[1],(p2x,p2y))
-    win.blit(finishline,(500,0))
+    win.blit(finishline,(800,0))
+    bfont = pygame.font.Font("freesansbold.ttf",50)
+    text = bfont.render('Start',True,WHITE)
+    #textSurf,button = text_objects("GO!", bfont)
+    #win.blit(textSurf, button)
     pygame.draw.rect(win, (200,0,200),startButton)
+    win.blit(text,(192,803))
     pygame.display.flip()
 
 
@@ -51,6 +58,7 @@ while run:
             run = False
         if ev.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
+            print(pos)
 
             #will start race
             if startButton.collidepoint(pos):
@@ -58,9 +66,9 @@ while run:
                 p1x,p2x = 0,0
 
     if racing == True:
-        if(p1x < 500 or p2x < 500):
-            p1x+=random.randint(0,5)
-            p2x+= random.randint(0,5)
+        if(p1x < 800 and p2x < 800):
+            p1x+=random.randint(0,1)
+            p2x+= random.randint(0,10)
         else:
             racing=False
 
